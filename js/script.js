88172963ee5c47667849ad740ffb3d0a1130ebc9
dygowski <dygowski@gmail.com>
2017-11-18 18:17:15 +0100

    Update JS to ES6

--- js/script.js
+++ js/script.js
@@ -1,153 +1,143 @@
-$(document).ready(function() {
-  var strict = false;
-  var power = false;
-  var pushable = false;
-  var soundUrl = "https://s3.amazonaws.com/freecodecamp/simonSound";
-  var combination = [];
-  var click = 0;
+$(document).ready(() => {
+  let strict = false;
+  let power = false;
+  let pushable = false;
+  const soundUrl = 'https://s3.amazonaws.com/freecodecamp/simonSound';
+  let combination = [];
+  let click = 0;
 
-  // functions
-  
+  // functions  
   // creating combination of buttons to press
   function createCombination() {
-    var randomNumber = Math.floor(Math.random() * 4 + 1);
+    const randomNumber = Math.floor((Math.random() * 4) + 1);
     combination.push(randomNumber);
-    }
+  }
 
-  //showing the combination
+  // showing the combination
   function showCombination(i) {
     if (combination.length < 10) {
-    $(".counter").html("0" + combination.length);
-    }
-    else {
-    $(".counter").html(combination.length);  
+      $('.counter').html(`0${combination.length}`);
+    } else {
+      $('.counter').html(combination.length);  
     }
     click = 0;
-    new Audio(soundUrl + combination[i] + ".mp3").play();
-      var autoId = "#" + combination[i];
-      $(autoId).addClass("push-active");
-      setTimeout(function() {
-        $(autoId).removeClass("push-active");
-        i++;
-        if (i < combination.length) {
-          showCombination(i);
-        }
-        else {
-          pushable = true;
-        }
-      }, 1000);
-    }
+    new Audio(`${soundUrl}${combination[i]}.mp3`).play();
+    const autoId = `#${combination[i]}`;
+    $(autoId).addClass('push-active');
+    setTimeout(() => {
+      $(autoId).removeClass('push-active');
+      i++;
+      if (i < combination.length) {
+        showCombination(i);
+      } else {
+        pushable = true;
+      }
+    }, 1000);
+  }
 
   // checking the clicks
   function checkClick(cl, id) {
     if (id === combination[cl - 1]) {
       return true;
-    }
-    else {
+    } else {
       return false;
     }
   }
-  
-  //restart game
-  
+
+  // restart game  
   function gameRestart() {
-   click = 0;
-      combination = [];
-      createCombination();
-      showCombination(0); 
+    click = 0;
+    combination = [];
+    createCombination();
+    showCombination(0);
   }
-  
+
   // victory
   function victory(e) {
-    var gratz = ["Y", "O", "U", "-","W", "I", "N", "!"];
-    var msg = gratz[e] + gratz[e + 1];
-     setTimeout(function() {
-    if (e < 7) {
-    $(".counter").html(msg);
-      e++;
-      victory(e);
-    }
-       else {
-         gameRestart();
-       }
-     }, 600); 
-    }
-    
+    const gratz = ['Y', 'O', 'U', '-', 'W', 'I', 'N', '!'];
+    const msg = gratz[e] + gratz[e + 1];
+    setTimeout(() => {
+      if (e < 7) {
+        $('.counter').html(msg);
+        e++;
+        victory(e);
+      } else {
+        gameRestart();
+      }
+    }, 600);
+  }
+
   // on-off
-  $(".pwr-but").click(function() {
+  $('.pwr-but').click(() => {
     power = !power;
     if (power) {
-      $(".pwr-but").addClass("pb-pushed");
-      $(".counter").html("00");
+      $('.pwr-but').addClass('pb-pushed');
+      $('.counter').html('00');
      } else {
-      $(".pwr-but").removeClass("pb-pushed");
-      $(".counter").html("");
+      $('.pwr-but').removeClass('pb-pushed');
+      $('.counter').html('');
       strict = false;
       pushable = false;
       click = 0;
       combination = [];
-      $("#lamp-light").removeClass("lamp-on");
+      $('#lamp-light').removeClass('lamp-on');
     }
   });
 
-  //game start
-  $(".start").click(function() {
+  // game start
+  $('.start').click(() => {
     if (power) {
       gameRestart();
     } // if power
   });
 
-  //strict mode
-  $(".strict").click(function() {
+  // strict mode
+  $('.strict').click(() => {
     if (power) {
       strict = !strict;
       if (strict) {
-        $("#lamp-light").addClass("lamp-on");
+        $('#lamp-light').addClass('lamp-on');
       } else {
-        $("#lamp-light").removeClass("lamp-on");
+        $('#lamp-light').removeClass('lamp-on');
       }
     } // if power
   });
 
   // your button pressing
 
-  $(".push").mousedown(function(e) {
+  $('.push').mousedown((e) => {
     if (power && pushable) {
-    click++;
-    pushable = false;
-    var yourId = Number(e.target.id);
-    if (checkClick(click, yourId)) {
-      new Audio(soundUrl + yourId + ".mp3").play();
-      if (click === combination.length) {
-        if (click === 20) {
-          new Audio("http://osamix.ct8.pl/victory.mp3").play();
-          victory(0);
+      click++;
+      pushable = false;
+      const yourId = Number(e.target.id);
+      if (checkClick(click, yourId)) {
+        new Audio(`${soundUrl}${yourId}.mp3`).play();
+        if (click === combination.length) {
+          if (click === 20) {
+            new Audio('http://osamix.ct8.pl/victory.mp3').play();
+            victory(0);
+          } else {
+            createCombination();
+            setTimeout(() => {
+              showCombination(0);
+            }, 1200);
+          }
+        } else {
+          pushable = true;
         }
-        else {
-        createCombination();
-        setTimeout(function() {
-        showCombination(0);
-        }, 1200);
-        }
-      }
-      else {
-        pushable = true;
-      }
-      }
-      else {
-      $(".counter").html("!!");
-        new Audio("http://osamix.ct8.pl/error.mp3").play();
+      } else {
+        $('.counter').html('!!');
+        new Audio('http://osamix.ct8.pl/error.mp3').play();
         if (!strict) {
-        setTimeout(function() {
-        showCombination(0);
-        }, 2000);
-      }
-      else {
-      setTimeout(function() {
-      $(".counter").html("00");
-      gameRestart();
-      }, 2000);
-      }
+          setTimeout(() => {
+            showCombination(0);
+          }, 2000);
+        } else {
+          setTimeout(() => {
+            $('.counter').html('00');
+            gameRestart();
+          }, 2000);
+        }
       }
     }
   });
